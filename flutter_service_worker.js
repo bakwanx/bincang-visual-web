33121df8aec702784737490ab6adb1f3fbcec33d
farhanfadhilah <farhanfadhilah87@gmail.com>
2025-07-05 23:41:05 +0700

    update ui

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a5d1eecb4cbfdb3f6b539439221b56cd",
+const RESOURCES = {"flutter_bootstrap.js": "218a905c45a68edf85dfa04aa861c1b7",
 "version.json": "fe7b4c2c4c34acda408d3c0b700c970b",
 "index.html": "f2ec39ca5727dd9efdaad00600a8a441",
 "/": "f2ec39ca5727dd9efdaad00600a8a441",
-"main.dart.js": "e67f9a7290b624ae4930f3689a769f34",
+"main.dart.js": "ba91d64fed312274775ce1b4e96bbd09",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
